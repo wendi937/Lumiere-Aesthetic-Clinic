8980d5c5347696968da8b7b4bcbb8e9a2d731d3d
Ajmi Capo <acapo23@epoka.edu.al>
2025-11-15 19:21:11 +0100

    Refactor navbar across multiple pages and add price list functionality

---
+++ js/price.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    $("#btn-view-prices").click(function () {
+
+        var modal = new bootstrap.Modal(document.getElementById("modalPrices"));
+
+        modal.show();
+    });
+
+});
